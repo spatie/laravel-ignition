5fcdc8c0e6284d9f0f36d22b9a3d8290964f39c0
freekmurze <freekmurze@users.noreply.github.com>
2021-05-26 15:30:07 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,13 +4,12 @@ namespace Spatie\LaravelIgnition\Tests;
 
 use Spatie\FlareClient\Glows\Glow;
 use Spatie\FlareClient\Report;
-use Spatie\LaravelIgnition\Facades\Flare;
 use Spatie\Ignition\Tests\TestClasses\FakeTime;
+use Spatie\LaravelIgnition\Facades\Flare;
 use Spatie\LaravelIgnition\IgnitionServiceProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-
     protected function getPackageProviders($app)
     {
         config()->set('flare.key', 'dummy-key');
