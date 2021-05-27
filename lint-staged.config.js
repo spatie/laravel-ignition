111026932257e158ddeceaf25e394df5bb392531
freek <freek@spatie.be>
2021-05-27 15:53:05 +0200

    remove js parts

--- lint-staged.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    'resources/{css,js}/**/*.{css,js,ts,tsx}': ['prettier --write'],
-};
--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: [
-        require('postcss-easy-import'),
-        require('tailwindcss')('./tailwind.config.js'),
-        require('postcss-preset-env')(),
-    ],
-};
