3aa6af43ab4a79413ad5127306c2cf66b8b3fe54
freekmurze <freekmurze@users.noreply.github.com>
2021-12-09 14:51:58 +0000

    Fix styling

--- src/Http/Requests/UpdateConfigRequest.php
+++ src/Http/Requests/UpdateConfigRequest.php
@@ -4,9 +4,6 @@ namespace Spatie\LaravelIgnition\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
-use Spatie\Ignition\Contracts\RunnableSolution;
-use Spatie\Ignition\Contracts\Solution;
-use Spatie\Ignition\Contracts\SolutionProviderRepository;
 
 class UpdateConfigRequest extends FormRequest
 {
