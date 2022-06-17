f6e1278941f27bbfc2ea27ed84fd796db6a3b655
Jess Archer <jess@jessarcher.com>
2022-06-17 14:57:33 +1000

    Add missing solution provider registration for Vite manifest

--- config/ignition.php
+++ config/ignition.php
@@ -12,6 +12,7 @@ use Spatie\LaravelIgnition\Solutions\SolutionProviders\MissingColumnSolutionProv
 use Spatie\LaravelIgnition\Solutions\SolutionProviders\MissingImportSolutionProvider;
 use Spatie\LaravelIgnition\Solutions\SolutionProviders\MissingLivewireComponentSolutionProvider;
 use Spatie\LaravelIgnition\Solutions\SolutionProviders\MissingMixManifestSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\MissingViteManifestSolutionProvider;
 use Spatie\LaravelIgnition\Solutions\SolutionProviders\RunningLaravelDuskInProductionProvider;
 use Spatie\LaravelIgnition\Solutions\SolutionProviders\TableNotFoundSolutionProvider;
 use Spatie\LaravelIgnition\Solutions\SolutionProviders\UndefinedViewVariableSolutionProvider;
@@ -106,6 +107,7 @@ return [
         MissingColumnSolutionProvider::class,
         UnknownValidationSolutionProvider::class,
         MissingMixManifestSolutionProvider::class,
+        MissingViteManifestSolutionProvider::class,
         MissingLivewireComponentSolutionProvider::class,
         UndefinedViewVariableSolutionProvider::class,
         GenericLaravelExceptionSolutionProvider::class,
