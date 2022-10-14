61b80c5eb3dd3f6179e5f9131f8b191244a01756
Enzo Innocenzi <enzo@innocenzi.dev>
2022-10-14 14:09:10 +0200

    feat: improve Vite solution

--- tests/Solutions/ViteManifestNotFoundSolutionProviderTest.php
+++ tests/Solutions/ViteManifestNotFoundSolutionProviderTest.php
@@ -10,10 +10,42 @@ it('can solve a missing Vite manifest exception', function () {
     expect($canSolve)->toBeTrue();
 });
 
-it('can recommend running npm install and npm run dev', function () {
+it('recommends running `npm run dev` in a local environment', function () {
+    app()->detectEnvironment(fn () => 'local');
+
     /** @var \Spatie\Ignition\Contracts\Solution $solution */
     $solution = app(MissingViteManifestSolutionProvider::class)
         ->getSolutions(new Exception('Vite manifest not found at: public/build/manifest.json'))[0];
 
-    expect(Str::contains($solution->getSolutionDescription(), 'Did you forget to run `npm install && npm run dev`?'))->toBeTrue();
+
+    expect(Str::contains($solution->getSolutionDescription(), 'Run `npm run dev` in your terminal and refresh the page.'))->toBeTrue();
 });
+
+it('recommends running `npm run build` in a production environment', function () {
+    app()->detectEnvironment(fn () => 'production');
+
+    /** @var \Spatie\Ignition\Contracts\Solution $solution */
+    $solution = app(MissingViteManifestSolutionProvider::class)
+        ->getSolutions(new Exception('Vite manifest not found at: public/build/manifest.json'))[0];
+
+
+    expect(Str::contains($solution->getSolutionDescription(), 'Run `npm run build` in your deployment script.'))->toBeTrue();
+});
+
+it('detects the package manager and adapts the recommended command', function (string $lockfile, string $command) {
+    app()->detectEnvironment(fn () => 'local');
+
+    file_put_contents(base_path($lockfile), '');
+
+    /** @var \Spatie\Ignition\Contracts\Solution $solution */
+    $solution = app(MissingViteManifestSolutionProvider::class)
+        ->getSolutions(new Exception('Vite manifest not found at: public/build/manifest.json'))[0];
+
+    expect(Str::contains($solution->getSolutionDescription(), "Run `{$command}` in your terminal and refresh the page."))->toBeTrue();
+
+    unlink(base_path($lockfile));
+})->with([
+    ['pnpm-lock.yaml', 'pnpm dev'],
+    ['yarn.lock', 'yarn dev'],
+    ['package-lock.json', 'npm run dev']
+]);
