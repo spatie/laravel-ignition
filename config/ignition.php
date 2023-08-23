2a79af93168aec39dee6decd9d14561bcf58ee7f
Jess Archer <jess@jessarcher.com>
2023-08-23 14:11:11 +1000

    Suggest `sail artisan` on network address error

--- config/ignition.php
+++ config/ignition.php
@@ -23,6 +23,7 @@ use Spatie\LaravelIgnition\Solutions\SolutionProviders\UndefinedViewVariableSolu
 use Spatie\LaravelIgnition\Solutions\SolutionProviders\UnknownValidationSolutionProvider;
 use Spatie\LaravelIgnition\Solutions\SolutionProviders\ViewNotFoundSolutionProvider;
 use Spatie\LaravelIgnition\Solutions\SolutionProviders\OpenAiSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\SailNetworkSolutionProvider;
 
 return [
 
@@ -117,6 +118,7 @@ return [
         UndefinedViewVariableSolutionProvider::class,
         GenericLaravelExceptionSolutionProvider::class,
         OpenAiSolutionProvider::class,
+        SailNetworkSolutionProvider::class,
     ],
 
     /*
