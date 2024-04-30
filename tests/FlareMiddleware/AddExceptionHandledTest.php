2fde331a66c0d27cb93785b9ac05991da7316cf3
Ruben Van Assche <rubenvanassche@gmail.com>
2024-04-30 15:51:47 +0200

    Add support for handling errors

---
+++ tests/FlareMiddleware/AddExceptionHandledTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Orchestra\Testbench\Exceptions\Handler;
+use Spatie\FlareClient\Report;
+use Spatie\LaravelIgnition\Facades\Flare;
+
+it('can see when an exception is handled, meaning it is reported', function (){
+    $handler = new class(app()) extends Handler {
+        public static Report $report;
+
+        public function report(Throwable $e)
+        {
+            self::$report = Flare::createReport($e);
+        }
+    };
+
+    app()->bind(ExceptionHandler::class, fn() => $handler);
+
+    $someTriggeredException = new Exception('This is a test exception');
+
+    report($someTriggeredException);
+
+    expect($handler::$report)->toBeInstanceOf(Report::class);
+    expect($handler::$report->toArray())
+        ->toHaveKey('handled', true);
+});
+
+it('will not mark an exception handled when it is not', function (){
+    $someTriggeredException = new Exception('This is a test exception');
+
+    $report = Flare::createReport($someTriggeredException);
+
+    expect($report->toArray())->toHaveKey('handled', null);
+});
